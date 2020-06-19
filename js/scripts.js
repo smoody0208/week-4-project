f8bff504f923023ff318a3a2fbc114c993829c40
Spencer Moody <spencer.moody@outlook.com>
2020-06-19 08:50:16 -0700

    Add premiumTopping array and standardTopping array, and restructure branching.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,25 @@
 // Business Logic ------
 
-function Pizza (pizzaSize, pizzaTopping) {
+function Pizza (pizzaSize) {
   this.pizzaSize = pizzaSize;
-  this.pizzaTopping = pizzaTopping;
+  this.standardTopping = [];
+  this.premiumTopping = [];
 }
 
 // Calculate pizza cost ------
 
 Pizza.prototype.pizzaPrice = function() {
   this.price = 10;
-  if (this.pizzaTopping === "premium") {
-    this.price += 2;
-  } else if (this.pizzaTopping === "regular") {
-    this.price += 1;
+  if (this.standardTopping.length === 0) {
+    this.price;
+  } else {
+    this.price += (this.standardTopping * 1);
   }
+  if (this.premiumTopping.length === 0) {
+    this.price;
+  } else {
+    this.price += (this.premiumTopping * 2);
+  }  
   if (this.pizzaSize === "small") {
     this.price += 1;
   } else if (this.pizzaSize === "medium") {
