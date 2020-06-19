2bdd0f31c7fd9a32a3eb8049c29accd1d1bb27ef
Spencer Moody <spencer.moody@outlook.com>
2020-06-19 08:33:18 -0700

    Create branching for pizzatoppings and pizza size to determin price of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,17 @@ function Pizza (pizzaSize, pizzaTopping) {
 
 Pizza.prototype.pizzaPrice = function() {
   this.price = 10;
+  if (this.pizzaTopping === "premium") {
+    this.price += 2;
+  } else if (this.pizzaTopping === "regular") {
+    this.price += 1;
+  }
+  if (this.pizzaSize === "small") {
+    this.price += 1;
+  } else if (this.pizzaSize === "medium") {
+    this.price += 3;
+  } else {
+    this.price += 4;
+  }
+  return this.price;
 }
